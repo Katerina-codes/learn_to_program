507f38205f46d87abd64d035201e1c7685b92db5
katerina-codes <katerinaldg@gmail.com>
2017-03-20 21:59:34 +0000

    Updated exercise. Includes triple goodbye needed to exit

--- chapter_6_exercise2.rb
+++ chapter_6_exercise2.rb
@@ -1,19 +1,18 @@
 def grandma_program
-
 response = gets.chomp
 
-  while response != "BYE"
-    if response == response.upcase
+  while (response * 3) != "BYE"
+    if response == "BYE" && gets.chomp == "BYE" && gets.chomp == "BYE"
+      exit
+    elsif response == response.upcase
       puts "NO, NOT SINCE #{rand(1930..1950)}"
       response = gets.chomp
-    elsif response == response.downcase
+    else
+      response == response.downcase
       puts "HUH?!  SPEAK UP, SONNY!"
       response = gets.chomp
-    else
-      puts "SAY SOMETHING!"  
     end
   end
-
 end
 
 grandma_program
